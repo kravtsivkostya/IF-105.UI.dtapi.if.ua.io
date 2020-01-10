103ec6c4d3f85e31d689b1f9ed2fa975bb9b88b3
KostyaKravtsiv <kostya.kravtsiv@gmail.com>
2020-01-10 11:17:04 +0200

    [ADD] added speciality module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentsComponent } from './students/students.component';
+import { SpecialityListComponent } from './speciality/speciality-list/speciality-list.component';
 
 
 const routes: Routes = [
-  { path: '', component: AdminComponent , 
-    children:[
-      { path: 'students', component: StudentsComponent }
+  {
+    path: '', component: AdminComponent,
+    children: [
+      { path: 'students', component: StudentsComponent },
+      { path: 'speciality', component: SpecialityListComponent }
       // Here you can add routes for your entities
-  ]}
+    ]
+  }
 ];
 @NgModule({
   declarations: [
     AdminComponent,
-    StudentsComponent
+    StudentsComponent,
+    SpecialityListComponent
   ],
   imports: [
     CommonModule,
